695fb719eb0496ae5eb00de49227a8dc4f8a24de
juanjuanjuanzi <864467609@qq.com>
2023-03-07 00:53:13 +0800

    Update 277_topu.py

--- 277_topu.py
+++ 277_topu.py
@@ -12,8 +12,9 @@ class Solution(object):
         """
         g=[[] for _ in range(n)]
         ind=[0]*n
+        outd=[0]*n
         for i in range(n):
-            if ind[i]>0:
+            if ind[i]>0 or outd[i]>0:
                 continue
             for j in range(n):
                 if i==j:
@@ -21,14 +22,19 @@ class Solution(object):
                 if knows(j,i):
                     g[i].append(j)
                     ind[j]+=1
+                else:
+                    outd[i]+=1
         #找到len==n-1.且ind==0的结点
         #print(g)
         for i in range(n):
-            if len(g[i])==(n-1) and ind[i]==0:
+            if len(g[i])==(n-1) and ind[i]==0 and outd[i]==0:
                 valid=True
+                
                 for v in g[i]:
                     if knows(i,v):
                         valid=False
+                        break
+                
                 if valid:
                     return i
         return -1
