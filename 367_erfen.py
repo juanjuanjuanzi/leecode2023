7c8d4ff7862a535aeabdc1b49de13e2dba12c0fd
juanjuanjuanzi <864467609@qq.com>
2023-04-27 20:31:52 +0800

    Update 367_erfen.py

--- 367_erfen.py
+++ 367_erfen.py
@@ -17,3 +17,38 @@ class Solution:
             return True
         else:
             return False
+        
+'''
+执行用时：
+28 ms
+, 在所有 Python3 提交中击败了
+96.37%
+的用户
+内存消耗：
+14.8 MB
+, 在所有 Python3 提交中击败了
+82.75%
+的用户
+'''
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left=1
+        right=num//2+1
+        while left<=right:
+            mid=(left+right)>>1
+                           
+            if mid*mid>num:
+                right=mid-1
+            elif mid*mid<num:
+                left=mid+1
+            else:
+                return True
+
+        '''
+        if left*left==num or (left+1)*(left+1)==num:# or (left-1)*(left-1)==num:
+            return True
+        else:
+            return False
+        '''
+        return False
+        
